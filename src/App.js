183519f4531893767b7c53dd91451513c8f9c408
손창범 <nuclearwinte@gmail.com>
2021-06-22 19:46:18 +0900

    gg

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
-  const [info, setInfo] = useState(0);
+  const [info, setInfo] = useState(null);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get("http://127.0.0.1:4000/", {
+        withCredentials: true,
+      });
+      setInfo(response.data);
+    } catch (e) {
+      alert(e);
+    }
+  };
+
+  useEffect(() => {
+    console.log(info);
+  }, [info]);
 
   return (
     <div className="App">
       <div>
-        <div>cookie</div>
-        <div>jwt</div>
+        <div>
+          <button onClick={fetchData}>get cookie</button>
+          <div>cookie</div>
+          <div>{info && info.number}</div>
+        </div>
+        <div>
+          <button onClick={fetchData}>get jwt</button>
+          <div>jwt</div>
+          <div>{info && info.number}</div>
+        </div>
       </div>
     </div>
   );
