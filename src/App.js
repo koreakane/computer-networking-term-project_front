bbbcab328ebac2ee1468ac36d866549285ca4c2d
손창범 <nuclearwinte@gmail.com>
2021-06-23 23:08:34 +0900

    gg

--- src/App.js
+++ src/App.js
@@ -3,14 +3,63 @@ import "./App.css";
 import axios from "axios";
 
 function App() {
-  const [info, setInfo] = useState(null);
+  const [info, setInfo] = useState({
+    id: 0,
+    name: "",
+    authorized: false,
+    counter: 0,
+  });
 
-  const fetchData = async () => {
+  const [token, setToken] = useState(null);
+
+  const getDataByCookie = async () => {
     try {
-      const response = await axios.get("http://127.0.0.1:4000/", {
+      const response = await axios.get("http://127.0.0.1:4000/get-cookie", {
         withCredentials: true,
       });
       setInfo(response.data);
+    } catch (e) {
+      alert(e);
+    }
+  };
+
+  const postDataByCookie = async (currentInfo) => {
+    try {
+      const response = await axios.post(
+        "http://127.0.0.1:4000/post-cookie",
+        { info_cookie: currentInfo },
+        { withCredentials: true }
+      );
+      console.log(response.data);
+    } catch (e) {
+      alert(e);
+    }
+  };
+
+  const getDataByJwt = async () => {
+    try {
+      const response = await axios.get("http://127.0.0.1:4000/get-jwt", {
+        withCredentials: false,
+      });
+      setInfo(response.data.info_jwt);
+      setToken(response.data.accessToken);
+      window.localStorage.setItem("data", response.data.accessToken);
+    } catch (e) {
+      alert(e);
+    }
+  };
+
+  const postDataByJwt = async (token, currentInfo) => {
+    try {
+      const response = await axios.post(
+        "http://127.0.0.1:4000/post-jwt",
+        {
+          token: token,
+          info_jwt: currentInfo,
+        },
+        { withCredentials: false }
+      );
+      console.log(response.data);
     } catch (e) {
       alert(e);
     }
@@ -24,14 +73,40 @@ function App() {
     <div className="App">
       <div>
         <div>
-          <button onClick={fetchData}>get cookie</button>
-          <div>cookie</div>
-          <div>{info && info.number}</div>
+          <button onClick={getDataByCookie}>get cookie</button>
+          <button onClick={() => postDataByCookie(info)}>post cookie</button>
+          <div>cookie : ${document.cookie}</div>
         </div>
         <div>
-          <button onClick={fetchData}>get jwt</button>
-          <div>jwt</div>
-          <div>{info && info.number}</div>
+          <button onClick={getDataByJwt}>get jwt</button>
+          <button onClick={() => postDataByJwt(token, info)}>post jwt</button>
+          <div>jwt : ${window.localStorage.getItem("data")}</div>
+        </div>
+        <div>id : {info && info.id}</div>
+        <div>
+          <button
+            onClick={() =>
+              setInfo({
+                ...info,
+                counter: info.counter + 1,
+              })
+            }
+          >
+            counter : {info && info.counter}
+          </button>
+        </div>
+        <div>
+          <div>name :</div>
+          <input
+            type="text"
+            value={info && info.name}
+            onChange={(e) =>
+              setInfo({
+                ...info,
+                name: e.target.value,
+              })
+            }
+          ></input>
         </div>
       </div>
     </div>
